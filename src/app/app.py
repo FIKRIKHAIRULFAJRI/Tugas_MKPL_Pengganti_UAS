bec41035a9023a3b44b736f01e5fa87d7e7fae9c
Adhithya Nur Fatah <fataah051@gmail.com>
2025-06-12 23:03:51 +0700

    Update app.py

--- src/app/app.py
+++ src/app/app.py
@@ -5,13 +5,16 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+
 def multiply(a, b):
     return a * b
+
 
 def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero.")
     return a / b
+
 
 if __name__ == "__main__":
     print("Simple Calculator Module is running.")
